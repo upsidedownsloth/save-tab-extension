9ce1dcb6d29ae5bb3eca1403ce836bbd316fc794
upsidedownsloth <skeletonquerida@gmail.com>
2022-09-15 21:46:02 +1000

    styling updated

--- chrome-extension.js
+++ chrome-extension.js
@@ -1,6 +1,6 @@
 //define btn properties here to get element by id from HTML and use addEventListerner to evoke a function when the button is click.
 // can remove js function from HTML and leave a cleaner code in HTML
-let myLeads = []
+let items = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
@@ -8,27 +8,27 @@ const deleteBtn = document.getElementById("delete-btn")
 const saveBtn = document.getElementById("tab-btn")
 
 //get the leads from the local storage
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const itemsFromLocalStorage = JSON.parse(localStorage.getItem("items"))
 
 // check if leads is empthy, if not run the code
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    //calling render function with myLeads as parameter
-    render(myLeads)
+if (itemsFromLocalStorage) {
+    items = itemsFromLocalStorage
+    //calling render function with items as parameter
+    render(items)
 }
 
 // render function now have a better reusability by passing parameters when called
-// now the function has no direct relationship with myLeads global variable. The relationship will only created when the function is called with myLeads as its parameter
+// now the function has no direct relationship with items global variable. The relationship will only created when the function is called with items as its parameter
 function render(leads) {
     let listItems = ""
     for (let i = 0; i < leads.length; i++) {
-        // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
         listItems += 
         //create a template string with ` string, use ${function} to escape the `
         `<li>
             <a target='_blank' href='${leads[i]}'>
             ${leads[i]}
             </a>
+            <button class="deleteThis" id="deleteThis${leads[i]}" onclick="deleteThis()"><i class="fas fa-minus"></i></button>
         </li>
         `
     }
@@ -39,38 +39,33 @@ function render(leads) {
 
 //addEventListernet, passing in the event "click"
 inputBtn.addEventListener("click", function () {
-    //push new inpu to the myLeads array
-    myLeads.push(inputEl.value)
+    //push new inpu to the items array
+    items.push(inputEl.value)
     //empty the input field
     inputEl.value = ""
     
-    // save myLeads array to localStorage
+    // save items array to localStorage
     // localStorage only works with string, here is turing the array to string
-    //                   (   Key   , Value                 )
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    //                   (   Key   , Value             )
+    localStorage.setItem("items", JSON.stringify(items))
 
-    render(myLeads)
+    render(items)
 })
 
 saveBtn.addEventListener("click", function(){
     //grabbing url from the current tab
     // this code will only work when live as an extension
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads))
-        render(myLeads)
+        items.push(tabs[0].url)
+        localStorage.setItem("items", JSON.stringify(items))
+        render(items)
     })
 })
 
 deleteBtn.addEventListener("dblclick", function(){
     localStorage.clear()
-    //setting myLeads back to empty
-    myLeads = []
+    //setting items back to empty
+    items = []
     //call renderleads to clear the URL list as it is now contain nothing
-    render(myLeads)
+    render(items)
 })
-
-
-
-
-
