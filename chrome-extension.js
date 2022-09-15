e5a64fea2156d239d03154aee73916ee82c99ef3
upsidedownsloth <skeletonquerida@gmail.com>
2022-09-16 08:37:09 +1000

    add delete button to each list, update DOM, and add input on enter

--- chrome-extension.js
+++ chrome-extension.js
@@ -1,9 +1,10 @@
 //define btn properties here to get element by id from HTML and use addEventListerner to evoke a function when the button is click.
 // can remove js function from HTML and leave a cleaner code in HTML
-let items = []
+let items = [] //current link items
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
+// const ulEl = document.getElementById("ul-el")
+const ulEl = document.getElementById("link-list")
 const deleteBtn = document.getElementById("delete-btn")
 const saveBtn = document.getElementById("tab-btn")
 
@@ -24,32 +25,25 @@ function render(leads) {
     for (let i = 0; i < leads.length; i++) {
         listItems += 
         //create a template string with ` string, use ${function} to escape the `
-        `<li>
-            <a target='_blank' href='${leads[i]}'>
-            ${leads[i]}
-            </a>
-            <button class="deleteThis" id="deleteThis${leads[i]}" onclick="deleteThis()"><i class="fas fa-minus"></i></button>
-        </li>
+        `<div class="eLink" id="${[i]}">
+            <a target='_blank' href='${leads[i]}'>${leads[i]}</a>
+            <button class="deleteThis" onclick="deleteThis(this)"><i class="fas fa-minus"></i></button>
+        </div>
         `
     }
+    //this.parentElement.id give the div id
     //using .innerHTML here instead of .textContent so that <li> will be rendered as HTML not text     
     // .innetHTML (DOM manipulation) comes with a cost. Should do it outside the loop
     ulEl.innerHTML = listItems
 }
 
 //addEventListernet, passing in the event "click"
-inputBtn.addEventListener("click", function () {
-    //push new inpu to the items array
-    items.push(inputEl.value)
-    //empty the input field
-    inputEl.value = ""
-    
-    // save items array to localStorage
-    // localStorage only works with string, here is turing the array to string
-    //                   (   Key   , Value             )
-    localStorage.setItem("items", JSON.stringify(items))
-
-    render(items)
+inputBtn.addEventListener("click", addURL)
+inputEl.addEventListener("keyup", function(e){
+    let mLink = inputEl.value
+    if(e.key == "Enter"){
+      addURL()
+    }
 })
 
 saveBtn.addEventListener("click", function(){
@@ -69,3 +63,28 @@ deleteBtn.addEventListener("dblclick", function(){
     //call renderleads to clear the URL list as it is now contain nothing
     render(items)
 })
+
+function addURL(){
+    //push new input to the items array
+    if(inputEl.value != ""){
+        items.push(inputEl.value)
+        //empty the input field after push
+        inputEl.value = ""
+        // save items array to localStorage
+        // localStorage only works with string, here is turing the array to string
+        //                   (   Key   , Value             )
+        localStorage.setItem("items", JSON.stringify(items))
+
+        render(items)
+    }
+}
+
+function deleteThis(thisId){
+    let rmLink = thisId.parentElement.id
+    let rmDiv = document.getElementById(rmLink) // this work fine
+    items.splice(rmLink, 1) //remove from rmLink index
+    rmDiv.parentNode.removeChild(rmDiv) //remove DOM
+    JSON.parse(localStorage.getItem("items")) //check item in local storage
+    localStorage.setItem("items", JSON.stringify(items)) //update local storage with current list in items
+    render(items) //update ID in DOM
+}
